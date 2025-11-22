73e138d4d6578c9211805444715972ab8d85d43c
CarolineVarsaga <carolinevarsaga@gmail.com>
2025-11-22 17:00:17 +0100

    :sparkles: imports component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import EmployeeList from './components/EmployeeList.vue';
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
@@ -12,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+  <EmployeeList /> 
 </template>
 
 <style scoped>
