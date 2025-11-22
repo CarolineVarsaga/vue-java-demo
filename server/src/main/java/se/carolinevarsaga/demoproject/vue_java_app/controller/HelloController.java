035dcf2c0ac7fd8d595252f893249f955e8a6639
CarolineVarsaga <carolinevarsaga@gmail.com>
2025-11-22 16:59:01 +0100

    :fire: removes cross origin

--- server/src/main/java/se/carolinevarsaga/demoproject/vue_java_app/controller/HelloController.java
+++ server/src/main/java/se/carolinevarsaga/demoproject/vue_java_app/controller/HelloController.java
@@ -2,9 +2,7 @@ package se.carolinevarsaga.demoproject.vue_java_app.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class HelloController {
 
