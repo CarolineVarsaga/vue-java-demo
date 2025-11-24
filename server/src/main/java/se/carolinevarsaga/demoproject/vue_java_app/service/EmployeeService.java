725dbde4f7da696c02665f125497602421a50d72
CarolineVarsaga <carolinevarsaga@gmail.com>
2025-11-24 08:55:13 +0100

    🤡adds more dummy data

--- server/src/main/java/se/carolinevarsaga/demoproject/vue_java_app/service/EmployeeService.java
+++ server/src/main/java/se/carolinevarsaga/demoproject/vue_java_app/service/EmployeeService.java
@@ -13,6 +13,23 @@ public class EmployeeService {
     employees.add(new Employee(1L, "Anna", "Svensson", "Utvecklare"));
     employees.add(new Employee(2L, "Erik", "Nilsson", "Projektledare"));
     employees.add(new Employee(3L, "Karin", "Larsson", "UX Designer"));
+    employees.add(new Employee(4L, "Kurt", "Berg", "Projektledare"));
+    employees.add(new Employee(5L, "Lotta", "Andersson", "UX Designer"));
+    employees.add(new Employee(6L, "Jerry", "Klar", "UX Designer"));
+    employees.add(new Employee(7L, "Sara", "Holm", "Utvecklare"));
+    employees.add(new Employee(8L, "Peter", "Johansson", "Testare"));
+    employees.add(new Employee(9L, "Eva", "Sundberg", "Utvecklare"));
+    employees.add(new Employee(10L, "Magnus", "Lind", "Projektledare"));
+    employees.add(new Employee(11L, "Carina", "Björk", "UX Designer"));
+    employees.add(new Employee(12L, "Thomas", "Hansson", "Testare"));
+    employees.add(new Employee(13L, "Linda", "Karlsson", "Utvecklare"));
+    employees.add(new Employee(14L, "Johan", "Fredriksson", "Projektledare"));
+    employees.add(new Employee(15L, "Maria", "Nyström", "UX Designer"));
+    employees.add(new Employee(16L, "Anders", "Olsson", "Utvecklare"));
+    employees.add(new Employee(17L, "Sofia", "Lund", "Testare"));
+    employees.add(new Employee(18L, "Björn", "Åberg", "Projektledare"));
+    employees.add(new Employee(19L, "Kristin", "Ek", "UX Designer"));
+    employees.add(new Employee(20L, "Henrik", "Pettersson", "Utvecklare"));
 
     return employees;
   }
