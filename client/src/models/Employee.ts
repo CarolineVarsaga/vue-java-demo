9dfce81a44654945aa70a0cbeeb18841b4d72859
CarolineVarsaga <carolinevarsaga@gmail.com>
2025-11-24 14:01:47 +0100

    :sparkles: adds interface for stats

--- client/src/models/Employee.ts
+++ client/src/models/Employee.ts
@@ -3,4 +3,11 @@ export interface IEmployee {
   firstName: string;
   lastName: string;
   position: string;
+  createdAt: string;
+}
+export interface IStats {
+  totalEmployees: number;
+  employeesPerPosition: Record<string, number>;
+  addedLast30Days: number;
+  top5Positions: string[];
 }
