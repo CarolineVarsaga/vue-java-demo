d67a5274155af2be489eb0c49bea2dc4c58eee34
CarolineVarsaga <carolinevarsaga@gmail.com>
2025-11-24 14:02:56 +0100

    :sparkles: adds stats controller

---
+++ server/src/main/java/se/carolinevarsaga/demoproject/vue_java_app/controller/EmployeeStatsController.java
@@ -0,0 +1,24 @@
+package se.carolinevarsaga.demoproject.vue_java_app.controller;
+
+import se.carolinevarsaga.demoproject.vue_java_app.DTO.EmployeeStatsDto;
+import se.carolinevarsaga.demoproject.vue_java_app.service.EmployeeService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/stats")
+public class EmployeeStatsController {
+
+  private final EmployeeService employeeService;
+
+  public EmployeeStatsController(EmployeeService employeeService) {
+    this.employeeService = employeeService;
+  }
+
+  @GetMapping
+  public EmployeeStatsDto getStats() {
+    return employeeService.getStatistics();
+  }
+}
